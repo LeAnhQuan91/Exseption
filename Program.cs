30e35ca07cd53dd9622a28225f6dd55587356228
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-04-23 20:37:36 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int [] myArray = {0,1,2,3,4,5,6,7,8,9};
+        
+    try
+    {
+      Console.WriteLine(myArray [11]);
+    }
+  catch (System.Exception ex)   
+  {
+    Console.WriteLine("Loi roi");
+  }
+    finallycl
+    {
+
+    }
+    int a = 10;
+    int b = 0;
+    try 
+    {
+        int c = a/b;
+    }
+    catch (FomatException ex)
+    {
+Console.WriteLine("exMessage");
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("exMessage");
+    }
+}
+}
